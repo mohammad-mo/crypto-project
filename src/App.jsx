d2974532d9033d7b9f570e0a91677a5b1f5fed98
Mohammad <mohammadmahone@gmail.com>
2022-05-01 00:50:03 +0430

    - Add toggle theme feature.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import Landing from './components/Landing'
 import ErrorBoundary from './components/ErrorBoundary'
+import { useState } from 'react'
 
 const App = () => {
+  const [theme, setTheme] = useState('')
+
   return (
     <ErrorBoundary>
-      <div className='flex flex-col justify-center max-w-7xl py-5 px-2 sm:px-10 mx-auto'>
-        <Landing />
+      <div
+        className='flex flex-col justify-center max-w-7xl py-5 px-2 sm:px-10 mx-auto'
+        data-theme={`${!theme ? 'dark' : 'light'}`}
+      >
+        <Landing theme={theme} setTheme={setTheme} />
       </div>
     </ErrorBoundary>
   )
