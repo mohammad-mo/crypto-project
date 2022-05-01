2e6b63414e34be07d1446b7077e9c812bebdc150
Mohammad <mohammadmahone@gmail.com>
2022-05-01 10:06:17 +0430

    - Request for more coins(200).

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const BASE_URL =
-  'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+  'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=200&page=1&sparkline=false'
 
 export const getCoin = async () => {
   try {
